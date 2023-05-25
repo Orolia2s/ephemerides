2ed144b633d6c855a32bc5ed3b081d369a75e2d6
Antoine <antoine.gagniere@orolia2s.com>
2023-05-25 11:41:08 +0200

    Start using a logger

--- gnss_parser/__main__.py
+++ gnss_parser/__main__.py
@@ -1,28 +1,32 @@
 import argparse
 import yaml
 import sys
+import logging
+import traceback
 
 from serial import Serial
 from pyubx2 import UBXReader
 
-from gnss_parser import GnssFormat, Constellation, ensure_fields
-
-message_from_ublox = {
-    (Constellation.GPS, 0): 'LNAV-L', # L1 C/A (Coarse Acquisition)
-}
+from gnss_parser import GnssFormat, ensure_fields
 
 def interpret(obj: dict[str]):
     ensure_fields('top level', obj, ['kind'])
     return {'GNSS_format': GnssFormat}[obj['kind']](obj)
 
 if __name__ == '__main__':
-    cli_parser = argparse.ArgumentParser(description='Parse ICD yaml files')
+    cli_parser = argparse.ArgumentParser(prog = 'Ephemerides',
+                                         description = 'Parse yaml ICD files, to generate code or parse ublox stream',
+                                         epilog = 'Orolia 2S')
     group = cli_parser.add_mutually_exclusive_group(required = True)
     group.add_argument('-f', '--file', type = str, help = 'a file to parse as ublox stream')
     group.add_argument('-s', '--serial', type = str, help = 'the serial port to listen to')
     group.add_argument('-o', '--output', choices = ['md'], help = 'Instead of parsing, generate to sdtin in the language provided')
     cli_parser.add_argument('files', metavar = 'FILE', type = str, nargs = '+', help = 'a yaml file to process')
+    cli_parser.add_argument('-v', '--verbose', action='store_true')
     cli_args = cli_parser.parse_args()
+
+    logging.basicConfig(level = logging.DEBUG if cli_args.verbose else logging.INFO)
+    logging.info(f'Starting Ephemerides Generator with arguments: {cli_args}')
 
     # Parse ICDs to define formats
     formats = {}
@@ -32,7 +36,7 @@ if __name__ == '__main__':
                 format_parser = interpret(yaml.safe_load(f))
                 formats[format_parser.message] = format_parser
         except Exception as err:
-            print(f'Invalid file "{file_name}":', err, file = sys.stderr)
+            logging.exception(err)
 
     # Read ublox stream using formats
     if cli_args.output:
@@ -45,42 +49,8 @@ if __name__ == '__main__':
         stream = open(cli_args.file, 'rb')
     reader = UBXReader(stream, protfilter = 2)
     for _, ublox_message in reader:
-        #if not hasattr(ublox_message, 'identity'):
-        #    continue
-        if ublox_message.identity == 'RXM-SFRBX':
-            constellation = Constellation(ublox_message.gnssId)
-            message = message_from_ublox.get((constellation, ublox_message.sigId), '(Unsupported)')
-            print(constellation, message)
-            if message in formats:
-                words = [getattr(ublox_message, f'dwrd_{i+1:02}') for i in range(ublox_message.numWords)]
-                total = 0
-                if message == 'LNAV-L':
-                    P29 = 0
-                    P30 = 0
-                    for word in words:
-                        print(f'{word:032b}')
-                        if P30:
-                            word = ~word & 0xffffffff
-                        print(f'{word:032b}')
-                        b25 = word & (0b111011000111110011010010 << 6)
-                        b26 = word & (0b011101100011111001101001 << 6)
-                        b27 = word & (0b101110110001111100110100 << 6)
-                        b28 = word & (0b010111011000111110011010 << 6)
-                        b29 = word & (0b101011101100011111001101 << 6)
-                        b30 = word & (0b001011011110101000100111 << 6)
-                        parity = sum((t.bit_count() & 1) << p for p, t in enumerate([b30 | P29,
-                                                                                     b29 | P30,
-                                                                                     b28 | P30,
-                                                                                     b27 | P29,
-                                                                                     b26 | P30,
-                                                                                     b25 | P29]))
-                        print(f'{parity: 32b}')
-                        P30 = parity & 1
-                        P29 = (parity & 2) >> 1
-                        total <<= 24
-                        total += (word >> 6) & 0xFFFFFF
-                    print(f'{total:060x}')
-                #try:
-                #formats[message].parse_ublox_subframe(ublox_message.payload[8:])
-                #except Exception as err:
-                #    print(f'Error when parsing message {message} ({constellation}):', err, file = sys.stderr)
+        try:
+            formats[message].parse_ublox_subframe()
+        except Exception as err:
+            logging.exception(err)
+            #print(f'Error when parsing message {message} ({constellation}):', err, file = sys.stderr)
