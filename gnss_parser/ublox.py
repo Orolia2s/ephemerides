ec37c4b55ee22234c9c5047de0a6ddba11151989
Antoine <antoine.gagniere@orolia2s.com>
2023-06-01 14:36:56 +0200

    No inversion needed thanks to ublox !

--- gnss_parser/ublox.py
+++ gnss_parser/ublox.py
@@ -19,16 +19,13 @@ def parity_LNAVL(byte_array: bytes) -> int:
     While the parity algorithm is GPS specific, the fact that words are given as
     32-bits little-endian integers with 2 bits of padding is ublox-specific.
     The 2 MSBs are not zero, they are the 2 LSBs of the last word.
-
-    Note that if the LSB of the previous word was 1, the whole word must be inverted.
+    Also, the 24 data bits require no inversion, it has already been done by ublox.
     """
     words = [int.from_bytes(four, 'little') for four in grouper(byte_array, 4, incomplete = 'strict')]
     total = 0
     previous_29 = 0
     previous_30 = 0
     for word in words:
-        if previous_30:
-            word = ~word & 0xffffffff
         b25 = word & (0b111011000111110011010010 << 6)
         b26 = word & (0b011101100011111001101001 << 6)
         b27 = word & (0b101110110001111100110100 << 6)
@@ -44,10 +41,13 @@ def parity_LNAVL(byte_array: bytes) -> int:
             b28 |= 1
             b29 |= 1
         parity = sum(xor_bits(t) << p for p, t in enumerate([b30, b29, b28, b27, b26, b25]))
-        if parity != word & 0x3F:
-            logging.warning(f'Wrong parity: {parity:06b} vs {word & 0x3f:06b}')
-        previous_30 = word & 1
-        previous_29 = (word & 2) >> 1
+        ublox_parity = word & 0x3f
+        if previous_30:
+            ublox_parity = ~ublox_parity & 0x3f
+        if parity != ublox_parity:
+            logging.warning(f'Wrong parity: {parity:06b} vs {ublox_parity & 0x3f:06b}')
+        previous_30 = parity & 1
+        previous_29 = (parity & 2) >> 1
         total <<= 24
         total += (word >> 6) & 0xFFFFFF
     return SingleWordBitReaderMsb(total, 10 * 24)
