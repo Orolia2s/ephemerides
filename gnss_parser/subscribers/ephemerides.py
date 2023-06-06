be72450777c45bbfe825cbc95a6c2c8a87d98923
Antoine <antoine.gagniere@orolia2s.com>
2023-06-06 18:11:00 +0200

    Provide translation for skydel

--- gnss_parser/subscribers/ephemerides.py
+++ gnss_parser/subscribers/ephemerides.py
@@ -1,5 +1,57 @@
 from types import SimpleNamespace
 
+'''
+SqrtA
+Eccentricity
+LittleOmega
+M0
+I0
+BigOmega
+DeltaN
+Idot
+BigOmegaDot
+
+Adot
+DeltaN0dot
+
+Cuc
+Cus
+Cic
+Cis
+Crc
+Crs
+
+ClockBias
+ClockDrift
+ClockDriftRate
+Tgd
+'''
+
+translate_to_skydel =
+{
+    'square_root_of_semi_major_axis': 'SqrtA',
+    'eccentricity': 'Eccentricity',
+    'argument_of_perigee': 'LittleOmega',
+    'mean_anomaly': 'M0',
+    'inclination_angle', 'I0',
+    'ascending_node_longitude': 'BigOmega',
+    'mean_motion_difference': 'DeltaN',
+    'rate_of_inclination_angle': 'Idot',
+    'rate_of_right_ascension': 'BigOmegaDot',
+
+    'argument_of_latitude_cosine_correction': 'Cuc',
+    'argument_of_latitude_sine_correction': 'Cus',
+    'inclination_angle_cosine_correction': 'Cic',
+    'inclination_angle_sine_correction': 'Cis',
+    'orbit_radius_cosine_correction': 'Crc',
+    'orbit_radius_sine_correction': 'Crs',
+
+    'clock_bias_correction': 'ClockBias',
+    'clock_drift_correction': 'ClockDrift',
+    'clock_drift_rate_correction': 'ClockDriftRate',
+    'group_delay_differential': 'Tgd'
+}
+
 def ephemeris(data: SimpleNamespace):
     print('Could create a Rinex:')
     for key, value in data.__dict__.items():
