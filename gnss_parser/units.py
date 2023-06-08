d149d72540aa19a23ce4c3ada7330a3dec0c1df2
Antoine <antoine.gagniere@orolia2s.com>
2023-06-08 17:46:05 +0200

    Add factor and units of time and ionospheric parameters

--- gnss_parser/units.py
+++ gnss_parser/units.py
@@ -5,5 +5,6 @@ Custom astropy units
 from astropy import units
 
 semicircle = units.def_unit('semicircle', 180 * units.deg)
+solar_flux = units.def_unit('sfu', 1e-22 * units.W / (units.Hz * units.m ** 2))
 
-units.add_enabled_units([semicircle])
+units.add_enabled_units([semicircle, solar_flux])
