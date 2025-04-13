bcb76a3fdc0c842e484e8deb6664f27d9a791220
Antoine <antoine.gagniere@orolia2s.com>
2025-04-13 18:21:58 +0200

    Call ublox reader from zig

--- utilz/build.zig
+++ utilz/build.zig
@@ -2,11 +2,19 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
 
-    _ = b.addModule("utilz", .{
+    const ublox = b.dependency("ublox_parser", .{ .target = target });
+    const bind = b.addTranslateC(.{
+        .root_source_file = ublox.path("include/ublox_reader.h"),
+        .target = target,
+        .optimize = .Debug,
+    });
+
+    const utils = b.addModule("utilz", .{
         .root_source_file = b.path("root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    utils.addImport("o2s", bind.createModule());
 }
--- utilz/root.zig
+++ utilz/root.zig
@@ -1,4 +1,6 @@
 const reader = @import("bit_reader.zig");
+const loop = @import("ublox_loop.zig");
 
 pub const SimpleBitReader = reader.SimpleBitReader;
 pub const SkippingBitReader = reader.SkippingBitReader;
+pub const ublox_reader_loop = loop.ublox_reader_loop;
