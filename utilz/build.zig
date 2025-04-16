4d933e28df8a8e9c60321fdc3a54e97cbd2607b8
Antoine <antoine.gagniere@orolia2s.com>
2025-04-16 21:23:24 +0200

    Add a direct dependency to libo2s from utilz

--- utilz/build.zig
+++ utilz/build.zig
@@ -5,11 +5,13 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
 
     const ublox = b.dependency("ublox_parser", .{ .target = target });
+    const o2s = b.dependency("libo2s", .{ .target = target });
     const bind = b.addTranslateC(.{
         .root_source_file = ublox.path("include/ublox_reader.h"),
         .target = target,
-        .optimize = .Debug,
+        .optimize = optimize,
     });
+    bind.addIncludePath(o2s.path("include"));
 
     const utils = b.addModule("utilz", .{
         .root_source_file = b.path("root.zig"),
@@ -17,4 +19,5 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     utils.addImport("o2s", bind.createModule());
+    utils.linkLibrary(ublox.artifact("ublox_parser"));
 }
