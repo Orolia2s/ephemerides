5f2b0b4cceba329a675918866ce80da626d1f0ee
Antoine <antoine.gagniere@orolia2s.com>
2025-04-26 23:49:30 +0200

    Add a mode to display how many subframes per satellite, grouped by constellation, were received

--- utilz/main.zig
+++ utilz/main.zig
@@ -8,6 +8,7 @@ const Mode = enum {
     header,
     message,
     constellation,
+    satellite,
 };
 
 const Options = struct {
@@ -21,7 +22,13 @@ const Options = struct {
         .full_text = "Parse ublox stream",
         .option_docs = .{
             .baudrate = "The specified path is a serial port and must be configured with this baudrate",
-            .mode = "What to output:\n\t\t\t'header' to dump class, type and length of all messages,\n\t\t\t'message' to show ublox content of all messages",
+            .mode =
+            \\What to output:
+            \\                      'header' to dump class, type and length of all messages,
+            \\                      'message' to show ublox content of all messages",
+            \\                      'constellation' to show how many subframe have been received per constellation,
+            \\                      'satellite' to show how many subframe have been received per satellite per constellation (2D table),
+            ,
             .help = "Print this help",
         },
     };
@@ -33,49 +40,75 @@ const out = buffered_out.writer();
 var gpa_instance: std.heap.GeneralPurposeAllocator(.{}) = .{};
 const gpa = gpa_instance.allocator();
 
+pub fn main() !void {
+    const options = try argsParser.parseForCurrentProcess(Options, gpa, .print);
+    defer options.deinit();
+
+    if (options.options.help) {
+        try argsParser.printHelp(Options, "ublox dumper", out);
+        try buffered_out.flush();
+        return;
+    }
+
+    for (options.positionals) |arg| {
+        if (options.options.baudrate) |baudrate| {
+            try utils.read_ublox_from(arg, .{ .is_serial_port = true, .baudrate = baudrate, .callback = get_callback(options.options.mode) });
+        } else {
+            try utils.read_ublox_from(arg, .{ .is_serial_port = false, .callback = get_callback(options.options.mode) });
+        }
+    }
+    try out.writeByte('\n');
+    try buffered_out.flush();
+}
+
 fn get_callback(mode: Mode) utils.UbloxCallback {
     return switch (mode) {
-        .header => dump_header,
-        .message => dump_message,
+        .header => c_dump_header,
+        .message => c_dump_message,
         .constellation => c_count_constellations,
+        .satellite => c_count_satellite,
     };
 }
 
-fn dump_header(c_message: [*c]o2s.ublox_message_t) callconv(.C) void {
-    const message: *o2s.ublox_message_t = c_message;
+fn c_dump_header(message: [*c]o2s.ublox_message_t) callconv(.C) void {
+    dump_header(message) catch unreachable;
+}
+fn dump_header(message: *o2s.ublox_message_t) !void {
     var string: o2s.string_t = o2s.ublox_header_tostring(message);
     defer o2s.string_clear(&string);
 
-    out.writeByte('{') catch unreachable;
-    out.print("{s}", .{o2s.string_to_cstring(&string)}) catch unreachable;
-    _ = out.write("}\n---\n") catch unreachable;
-    buffered_out.flush() catch unreachable;
+    try out.writeByte('{');
+    try out.print("{s}", .{o2s.string_to_cstring(&string)});
+    try out.writeAll("}\n---\n");
+    try buffered_out.flush();
 }
 
-fn dump_message(c_message: [*c]o2s.ublox_message_t) callconv(.C) void {
-    const message: *o2s.ublox_message_t = c_message;
+fn c_dump_message(message: [*c]o2s.ublox_message_t) callconv(.C) void {
+    dump_message(message) catch unreachable;
+}
+fn dump_message(message: *o2s.ublox_message_t) !void {
     var string: o2s.string_t = outer: switch (message.ublox_class) {
         o2s.MON => switch (message.type) {
-            o2s.HW => o2s.ublox_monitoring_hardware_tostring(@ptrCast(c_message)),
-            o2s.RF => o2s.ublox_monitoring_rf_tostring(@ptrCast(c_message)),
+            o2s.HW => o2s.ublox_monitoring_hardware_tostring(@ptrCast(message)),
+            o2s.RF => o2s.ublox_monitoring_rf_tostring(@ptrCast(message)),
             else => continue :outer 0,
         },
         o2s.RXM => switch (message.type) {
-            o2s.SFRBX => o2s.ublox_navigation_data_tostring(@ptrCast(c_message)),
+            o2s.SFRBX => o2s.ublox_navigation_data_tostring(@ptrCast(message)),
             else => continue :outer 0,
         },
         o2s.NAV => switch (message.type) {
-            o2s.PVT => o2s.ublox_position_time_tostring(@ptrCast(c_message)),
+            o2s.PVT => o2s.ublox_position_time_tostring(@ptrCast(message)),
             else => continue :outer 0,
         },
         else => o2s.ublox_header_tostring(message),
     };
     defer o2s.string_clear(&string);
 
-    out.writeByte('{') catch unreachable;
-    out.print("{s}", .{o2s.string_to_cstring(&string)}) catch unreachable;
-    out.writeAll("}\n---\n") catch unreachable;
-    buffered_out.flush() catch unreachable;
+    try out.writeByte('{');
+    try out.print("{s}", .{o2s.string_to_cstring(&string)});
+    try out.writeAll("}\n---\n");
+    try buffered_out.flush();
 }
 
 var constellation_count: std.AutoHashMap(o2s.ublox_constellation, u32) = .init(gpa);
@@ -95,29 +128,42 @@ fn count_constellations(subframe: *o2s.struct_ublox_navigation_data) !void {
     var ite = constellation_count.iterator();
     while (ite.next()) |entry| {
         try terminal.format.updateStyle(out, .{ .font_style = .{ .bold = entry.key_ptr.* == subframe.constellation } }, null);
-        try out.print("{s}: {}, ", .{ o2s.ublox_constellation_to_cstring(entry.key_ptr.*), entry.value_ptr.* });
+        try out.print("{s}: {}  ", .{ o2s.ublox_constellation_to_cstring(entry.key_ptr.*), entry.value_ptr.* });
     }
-    //try out.writeByte('\n');
     try buffered_out.flush();
 }
 
-pub fn main() !void {
-    defer _ = gpa_instance.deinit();
+var satellite_count: std.AutoArrayHashMapUnmanaged(o2s.ublox_constellation, std.AutoArrayHashMapUnmanaged(u8, u32)) = .empty;
 
-    const options = try argsParser.parseForCurrentProcess(Options, gpa, .print);
-    defer options.deinit();
-
-    if (options.options.help) {
-        try argsParser.printHelp(Options, "ublox dumper", out);
-        try buffered_out.flush();
+fn c_count_satellite(message: [*c]o2s.ublox_message_t) callconv(.C) void {
+    if (message.*.ublox_class != o2s.RXM or message.*.type != o2s.SFRBX)
         return;
+    count_satellite(@ptrCast(message)) catch unreachable;
+}
+fn count_satellite(subframe: *o2s.struct_ublox_navigation_data) !void {
+    {
+        const constellation = try satellite_count.getOrPutValue(gpa, subframe.constellation, .empty);
+        const satellite = try constellation.value_ptr.getOrPutValue(gpa, subframe.satellite, 0);
+        satellite.value_ptr.* += 1;
     }
 
-    for (options.positionals) |arg| {
-        if (options.options.baudrate) |baudrate| {
-            try utils.read_ublox_from(arg, .{ .is_serial_port = true, .baudrate = baudrate, .callback = get_callback(options.options.mode) });
-        } else {
-            try utils.read_ublox_from(arg, .{ .is_serial_port = false, .callback = get_callback(options.options.mode) });
+    try terminal.cursor.setCursorColumn(out, 0);
+    var outer = satellite_count.iterator();
+    while (outer.next()) |constellation| {
+        try terminal.cursor.saveCursor(out);
+        var inner = constellation.value_ptr.iterator();
+        while (inner.next()) |satellite| {
+            try terminal.cursor.cursorDown(out, 1);
+            try terminal.format.updateStyle(out, .{ .font_style = .{
+                .bold = (constellation.key_ptr.* == subframe.constellation and satellite.key_ptr.* == subframe.satellite),
+                .underline = false,
+            } }, null);
+            try out.print("{:02}: {:3}", .{ satellite.key_ptr.*, satellite.value_ptr.* });
+            try terminal.cursor.cursorBackward(out, 7);
         }
+        try terminal.cursor.restoreCursor(out);
+        try terminal.format.updateStyle(out, .{ .font_style = .{ .underline = true } }, null);
+        try out.print("{s:<12}", .{o2s.ublox_constellation_to_cstring(constellation.key_ptr.*)});
     }
+    try buffered_out.flush();
 }
